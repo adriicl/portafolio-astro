59de54a7c161b37e2712e6677c3e1f662e8acaf6
Adrian <adriancabreraleonis172003@gmail.com>
2025-02-06 13:34:19 +0100

    Agregar página de proyectos, actualizar navegación y mejorar estilos globales

--- src/scripts/nav.js
+++ src/scripts/nav.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const currentPath = window.location.pathname;
     document.querySelectorAll("nav a").forEach((link) => {
       if (link.getAttribute("data-link") === currentPath) {
-        link.classList.add("text-yellow-400", "border-b-2", "border-yellow-400");
+        link.classList.add("text-green-400", "border-b-2", "border-green-400");
       }
     });
   });
